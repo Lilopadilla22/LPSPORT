da51d2e4e77689e7a4deeca2d527eec20c0a5375
Lilia Padilla <lilopadilla22@gmail.com>
2025-04-05 13:37:50 -0500

    add context user in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import RootNavigator from './src/navigation/RootNavigator';
+import { UserProvider } from './src/store/context/userContext';
 
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <RootNavigator />
+      <UserProvider>
+        <RootNavigator />
+      </UserProvider>
     </SafeAreaProvider>
   );
 }
