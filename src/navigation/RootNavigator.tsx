42f51b393c950a607a245ed804a8b491e5b7ea55
Lilia Padilla <lilopadilla22@gmail.com>
2025-04-05 13:41:46 -0500

    fix in routs property

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -4,17 +4,27 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import WelcomeScreen from '../screens/Welcome/Welcome';
 import AuthStack from './authStack';
+import { useUser } from '../store/context/userContext';
+import HomeScreen from '../screens/home/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
+  const { user } = useUser();
 
+  console.log(user, 'usuario🔴🔴🔴🔴');
 
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-      <Stack.Screen name="AuthStack" component={AuthStack} />
+        {user ? (
+          <Stack.Screen name="AppTabs" component={HomeScreen} />
+        ) : (
+          <>
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="AuthStack" component={AuthStack} />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
