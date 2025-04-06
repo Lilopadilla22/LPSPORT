36def3e4ba98d82985e704820fcd36811d6cea11
Lilia Padilla <lilopadilla22@gmail.com>
2025-04-05 22:29:10 -0500

    feat app Tabs

---
+++ src/navigation/AppTabs.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import LoginScreen from '../screens/Auth/login/LoginScreen';
+import HomeScreen from '../screens/Home/HomeScreen';
+import AccountIcon from '../icons/AccountIcon';
+import HomeIcon from '../icons/HomeIcon';
+import BalloonIcon from '../icons/BalloonIcon';
+
+const Tab = createBottomTabNavigator();
+
+export default function AppTabs() {
+
+  const renderAccountIcon = ({ focused }: { focused: boolean }) => (
+    <AccountIcon fill={focused ? '#000' : '#AAA'} width={24} height={24}/>
+  );
+  const renderHomeIcon = ({ focused }: { focused: boolean }) => (
+    <HomeIcon fill={focused ? '#000' : '#AAA'} width={24} height={24}/>
+  );
+
+  const renderBallonIcon = ({ focused }: { focused: boolean }) => (
+    <BalloonIcon fill={focused ? '#000' : '#aaa'} width={24} height={24} />
+  );
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#aaa',
+        tabBarLabelStyle: { fontWeight: 'bold', fontSize: 12 },
+        tabBarStyle: {
+          backgroundColor: '#fff',
+          borderTopWidth: 0,
+          elevation: 0,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Inicio',
+          tabBarIcon: renderHomeIcon,
+        }}
+      />
+      <Tab.Screen
+        name="Matches"
+        component={LoginScreen}
+        options={{
+          tabBarLabel: 'Partidos',
+          tabBarIcon: renderBallonIcon,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={LoginScreen}
+        options={{
+          tabBarLabel: 'Perfil',
+          tabBarIcon: renderAccountIcon,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
