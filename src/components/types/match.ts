50b19fc4f6ee45152dae78b255a46ebde3a0aa72
Lilia Padilla <lilopadilla22@gmail.com>
2025-04-06 16:29:43 -0500

    add data

---
+++ src/components/types/match.ts
@@ -0,0 +1,12 @@
+export type Match = {
+  id: string;
+  title: string;
+  city: string;
+  distance: number;
+  date: string;
+  level: 'novato' | 'intermedio' | 'avanzado';
+  players: number;
+  maxPlayers: number;
+  location: string;
+  image: any;
+};
