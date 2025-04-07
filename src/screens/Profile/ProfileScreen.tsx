c0713da29c3e9a1c412f8876292ac5d6ccc7176b
Lilia Padilla <lilopadilla22@gmail.com>
2025-04-07 14:10:49 -0500

    update profile

--- src/screens/Profile/ProfileScreen.tsx
+++ src/screens/Profile/ProfileScreen.tsx
@@ -1,45 +1,43 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { useUser } from '../../store/context/userContext';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useEffect } from 'react';
+import CustomHeader from '../../components/CustomHeader';
+import GuestProfileScreen from './GuestProfileScreen';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import ProfileProgressBar from './Components/ProfileProgressBar';
 
+export type ProfileParamList = {
+  Home: undefined;
+  Account: undefined;
+};
 
-const ProfileScreen = () => {
-
+export default function ProfileScreen() {
   const { user, logout } = useUser();
+  const navigation = useNavigation<NativeStackNavigationProp<ProfileParamList>>();
 
-  useEffect(() => {
-    const checkStorage = async () => {
-      try {
-        const keys = await AsyncStorage.getAllKeys();
-        const items = await AsyncStorage.multiGet(keys);
+  if (!user || user.isGuest) {
+    return <GuestProfileScreen />;
+  }
 
-        console.log('consultanding', keys);
-        items.forEach(([key, value]) => {
-          console.log(`[${key}]:`, JSON.parse(value || 'null'));
-        });
-      } catch (e) {
-        console.error('errooooooor', e);
-      }
-    };
-
-    checkStorage();
-  }, []);
-
-  console.log(user, 'Hola')
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Bienvenido {user?.isGuest ? 'invitado' : user?.displayName ?? 'usuario'} 👋
-      </Text>
+      <CustomHeader logo={null} city={user.city} title={'Mi cuenta'} />
+      <Text style={styles.greeting}>Hola, {user.displayName}</Text>
 
-      <Text style={styles.subtext}>
-        {user?.isGuest
-          ? 'Estás navegando en modo invitado.'
-          : 'Has iniciado sesión correctamente.'}
-      </Text>
+      <Image
+        source={require('../../../assets/logo.png')}
+        style={styles.logo}
+        resizeMode="contain"
+      />
 
-      <TouchableOpacity style={styles.button} onPress={logout}>
+      <ProfileProgressBar user={user} />
+
+      <TouchableOpacity style={styles.buttonOutline} onPress={() => navigation.navigate('Account')}>
+        <Text style={styles.buttonOutlineText}>Mi cuenta</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonPrimary} onPress={logout}>
         <Text style={styles.buttonText}>Cerrar sesión</Text>
       </TouchableOpacity>
     </View>
@@ -49,31 +47,46 @@ const ProfileScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 24,
+    padding: 16,
   },
-  title: {
-    fontSize: 22,
+  greeting: {
+    fontSize: 20,
     fontWeight: 'bold',
+    marginTop: 30,
     marginBottom: 8,
   },
-  subtext: {
-    fontSize: 14,
-    color: '#666',
-    marginBottom: 30,
-    textAlign: 'center',
+  sectionTitle: {
+    fontSize: 16,
+    marginVertical: 12,
+    fontWeight: '600',
   },
-  button: {
+  logo: {
+    width: 120,
+    height: 120,
+    borderRadius: 70,
+    overflow: 'hidden',
+    alignSelf: 'center',
+    marginBottom: 24,
+    backgroundColor: '#fff',
+  },
+  buttonPrimary: {
     backgroundColor: '#000000',
-    paddingHorizontal: 24,
-    paddingVertical: 12,
+    padding: 14,
     borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
   },
-  buttonText: {
-    color: '#fff',
+  buttonText: { color: '#fff', fontWeight: 'bold' },
+  buttonOutline: {
+    borderColor: '#000000',
+    borderWidth: 2,
+    padding: 14,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 14,
+  },
+  buttonOutlineText: {
+    color: '#000000',
     fontWeight: 'bold',
   },
 });
-
-export default ProfileScreen
