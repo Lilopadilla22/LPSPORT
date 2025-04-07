133876fa97ee6f2b77f1d20d746391facf55885d
Lilia Padilla <lilopadilla22@gmail.com>
2025-04-06 22:26:48 -0500

    update MatchCard

--- src/components/MatchCart.tsx
+++ src/components/MatchCart.tsx
@@ -2,18 +2,34 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { Match } from './types/match';
 
-
 type MatchCardProps = {
   match: Match;
+  variant: 'complex' | 'search' | 'matchmaking';
 };
 
-const MatchCard = ({ match }: MatchCardProps) => {
+const MatchCard = ({ match, variant }: MatchCardProps) => {
   return (
     <View style={styles.card}>
       <Image source={match.image} style={styles.image} />
       <Text style={styles.title}>{match.title}</Text>
-      <Text style={styles.detail}>{match.city} - {match.distance} km</Text>
-      <Text style={styles.level}>Nivel: {match.level}</Text>
+
+      {variant === 'complex' && (
+        <>
+          <Text style={styles.detail}>{match.city}</Text>
+          <Text style={styles.detail}>{match.distance} km</Text>
+        </>
+      )}
+
+      {variant === 'search' && (
+        <>
+          <Text style={styles.detail}>Ranking requerido: {match.level}</Text>
+          <Text style={styles.detail}>Jugadores: {match.players}/{match.maxPlayers}</Text>
+        </>
+      )}
+
+      {variant === 'matchmaking' && (
+        <Text style={styles.detail}>Nivel: {match.level}</Text>
+      )}
     </View>
   );
 };
@@ -21,37 +37,29 @@ const MatchCard = ({ match }: MatchCardProps) => {
 const styles = StyleSheet.create({
   card: {
     width: 220,
-    height: 280,
+    height: 250,
     borderRadius: 12,
     overflow: 'hidden',
     marginRight: 12,
     backgroundColor: '#fff',
-    elevation: 3,
     marginTop: 8,
   },
   image: {
     width: '100%',
-    height: 150,
-    margin: 10,
+    height: 160,
+    marginBottom: 8,
   },
   title: {
     fontWeight: 'bold',
     fontSize: 18,
-    marginTop: 20,
     paddingHorizontal: 8,
+    marginBottom: 4,
   },
   detail: {
     fontSize: 15,
     color: '#666',
     paddingHorizontal: 8,
-    marginTop: 2,
-  },
-  level: {
-    fontSize: 14,
-    color: '#444',
-    marginTop: 2,
-    paddingHorizontal: 8,
-    marginBottom: 6,
+    marginBottom: 4,
   },
 });
 
